34ece1a1720f593760b59e89dd3872027aad8f42
strider <strider@Ubuntu.myguest.virtualbox.org>
2023-03-30 04:29:36 +1100

    basic scheduling

--- Client.java
+++ Client.java
@@ -10,12 +10,15 @@ public class Client {
     private static DataOutputStream output;
     private static BufferedReader input;
     
-    private static void receiveMsg() {
+    private static String receiveMsg() {
         try {
-            System.out.println(input.readLine());
+            String receivedMsg = input.readLine();
+            System.out.println(receivedMsg);
+            return receivedMsg;
         } catch (Exception e) {
             System.out.println(e);
         }
+        return null;
     }
     
     private static void sendMsg(String msg) {
@@ -25,7 +28,7 @@ public class Client {
         } catch (Exception e) {
             System.out.println(e);
         }
-    }   
+    }
     
     public static void main(String argv[]) {
         try {
@@ -48,6 +51,63 @@ public class Client {
             sendMsg("REDY\n");
             
             receiveMsg();
+            
+            sendMsg("GETS All\n");
+            String dataMsg = receiveMsg();
+            sendMsg("OK\n");
+            
+            // create a server array
+            
+            String num = "";
+            int counter = 0;
+            for (int i = 0; i < dataMsg.length(); i++) {
+                if (dataMsg.charAt(i) >= 48 && dataMsg.charAt(i) <= 57) {
+                    num += dataMsg.charAt(i);
+                } else if (dataMsg.charAt(i) == 32) {
+                    counter++;
+                    if (counter >= 2) { break; }
+                }
+            }
+                
+            int numOfServers = Integer.parseInt(num);
+            
+            String[] msgs = new String[numOfServers];
+            String[] servers = new String[numOfServers];
+            System.out.println(numOfServers);
+            for (int i = 0; i < numOfServers; i++) {
+                msgs[i] = input.readLine();
+            }
+            for (int i = 0; i < numOfServers; i++) {
+                System.out.println(msgs[i]);
+            }
+            
+            // end // create a server array
+            
+            sendMsg("OK\n");
+//            int k = 0;
+//            while (k <= 10) {//!receiveMsg().equals("NONE\n")) {
+//                for (int i = numOfServers, j = 0; i >= 0; i--, j++) {
+//                   sendMsg("REDY\n");
+//                    sendMsg("SCHD " + j + " joon 0\n");
+//                    receiveMsg();
+//                    k++;
+//                }   
+//            }
+            int j = 0;
+            while (j <= 20) {//!receiveMsg().equals("NONE\n")) {
+                for (int i = numOfServers; i >= 2; i--) {
+                    sendMsg("REDY\n");
+                    sendMsg("SCHD " + j + " joon 0\n");
+                    if (receiveMsg().contains("JCPL")) { break; }
+                    j++;
+                }
+                if (receiveMsg().contains("JCPL")) { break; }
+            }
+                
+                
+            
+            //sendMsg("SCHD 0 xlarge 0\n");
+            //receiveMsg();
             
             // Quit
             sendMsg("QUIT\n");
